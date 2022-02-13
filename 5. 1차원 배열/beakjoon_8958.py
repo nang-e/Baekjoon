a8784dfaffaa43361019105ef5bc6f3dee52df3c
nang-e <yeanvely@naver.com>
2022-02-13 23:59:14 +0900

    2022-02-13

--- 5. 1차원 배열/beakjoon_8958.py
+++ 5. 1차원 배열/beakjoon_8958.py
@@ -1,1 +1,6 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
+for i in range(n):
+    line = sys.stdin.readline().strip().split('X')
+    case = list(filter(None, line))
+    print(sum(list(map(lambda x: int((1+len(x))/2*len(x)), case))))

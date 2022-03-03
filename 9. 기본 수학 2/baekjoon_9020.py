58b0a14b8c23ff70985cff73b914dba1d4ec99c2
nang-e <yeanvely@naver.com>
2022-03-03 16:36:30 +0900

    2022-03-03

--- 9. 기본 수학 2/baekjoon_9020.py
+++ 9. 기본 수학 2/baekjoon_9020.py
@@ -1,3 +1,4 @@
+# 최종
 def prime(n):
     lst=[True]*n
     for i in range(2,int(n**1/2)+1):
@@ -15,3 +16,21 @@ for _ in range(T):
         if (k in lst) & ((n-k) in lst):
             print(k,n-k)
             break
+
+# 속도 개선
+sieve=[1]*10001
+sieve[0],sieve[1]=0,0
+for i in range(2,101):
+    if sieve[i]:
+        for j in range(i*i,10001,i):
+            sieve[j]=0
+
+for _ in range(int(input())):
+    n=int(input())
+    a=n//2
+    while 1:
+        if sieve[a]&sieve[n-a]:
+            print(a,n-a)
+            break
+        else:
+            a-=1

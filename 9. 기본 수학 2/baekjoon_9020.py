4cb2eaa8c9536a138017aec5211b79c94fa9273a
nang-e <yeanvely@naver.com>
2022-03-03 16:07:42 +0900

    2022-03-03

--- 9. 기본 수학 2/baekjoon_9020.py
+++ 9. 기본 수학 2/baekjoon_9020.py
@@ -1,4 +1,3 @@
-import sys
 def prime(n):
     lst=[True]*n
     for i in range(2,int(n**1/2)+1):
@@ -7,28 +6,12 @@ def prime(n):
                 lst[j]=False
     return [i for i in range(2,n) if lst[i]]
 
-for _ in range(int(input())):
+T=int(input())
+for _ in range(T):
     n=int(input())
     lst=prime(n)
-    if n/2 in lst:
-        print(int(n/2),int(n/2))
-    elif len(lst)%2:
-        for k in range(int(len(lst)/2)+1,-1,-1):
-            a,b=lst[k-1],n-lst[k-1]
-            if (a in lst)&(b in lst):
-                print(a,b)
-                break
-    else:
-        for k in range(int(len(lst)/2),-1,-1):
-            a,b=lst[k-1],n-lst[k-1]
-            if (a in lst)&(b in lst):
-                print(a,b)
-                break
-
-    # while i>=2:
-    #     if prime(i)&prime(n-i):
-    #         a,b=i,n-i
-    #         break
-    #     else:
-    #         i-=1
-    # print(a,b)
+    idx=max([i for i in lst if i<=(n/2)])
+    for k in range(idx,-1,-1):
+        if (k in lst) & ((n-k) in lst):
+            print(k,n-k)
+            break

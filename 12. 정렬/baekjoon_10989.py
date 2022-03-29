8c77731a0d5fe6765c6d2aad40289f183e509906
nang-e <yeanvely@naver.com>
2022-03-29 23:18:21 +0900

    2022-03-29

--- 12. 정렬/baekjoon_10989.py
+++ 12. 정렬/baekjoon_10989.py
@@ -1,7 +1,12 @@
-n=int(input())
-lst=[]
+import sys
+s=sys.stdin.readline
+lst=[0]*10001
+n=int(s())
 for _ in range(n):
-  lst.append(int(input()))
-lst.sort()
-
-print(*lst,sep='\n')
+    num = int(s())
+    lst[num] += 1
+    
+for i in range(10001):
+    if lst[i]:
+        for _ in range(lst[i]):
+            print(i)
--- 12. 정렬/baekjoon_2108.py
+++ 12. 정렬/baekjoon_2108.py
@@ -1,27 +1,17 @@
-import syslog
-t=sys.stdin.readline
-n=int(t())
-lst=[int(t()) for _ in range(n)] ; lst.sort()
-s=sorted(set(lst))
+from collections import Counter as c
+import sys
 
+s=sys.stdin.readline
+n=int(s())
+lst=[int(s()) for _ in range(n)] ; lst.sort()
 def mode(lst):
-  dic={}
-  for i in s:
-    dic[i]=lst.count(i)
-  cnt=0
-  for k,v in dic.items():
-    if (list(dic.values()).count(max(dic.values()))>=2)&(v==max(dic.values())):
-      cnt+=1
-      if cnt==2:
-        q3=k
-        return q3
-    elif (list(dic.values()).count(max(dic.values()))==1)&(v==max(dic.values())):
-      q3=k
-      return q3
-
+    m=c(lst).most_common()
+    if (len(m)>1):
+        if (m[0][1]==m[1][1]):
+            return m[1][0]
+    return m[0][0]
 q1=round(sum(lst)/n)
 q2=lst[int((n+1)/2)-1]
 q3=mode(lst)
 q4=lst[-1]-lst[0]
-
 print(q1,q2,q3,q4,sep='\n')
